620f2ade11e4f49150c0702cb706c3738a8d7889
Duy <1659007@itec.hcmus.edu.vn>
2023-05-05 15:53:37 +0700

    correct backend endpoint

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,4 +1,4 @@
-export const BASE_API_URL = 'https://13.41.81.179:8080';
+export const BASE_API_URL = 'https://server.atdang.com';
 // export const BASE_API_URL = 'https://34.216.75.142:8080';
 
 export const FILESTACK_API_KEY = 'AhQquDPbSqy9Fyi3pOVBbz';
